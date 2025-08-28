0daa8559a1e1cd8c950b48f526560a27205b3730
bgoRelatel <bgo@relatel.dk>
2025-08-28 12:39:10 +0200

    less configuration more signicat specific

--- lib/omniauth/strategies/signicat_oauth2.rb
+++ lib/omniauth/strategies/signicat_oauth2.rb
@@ -4,10 +4,17 @@ module OmniAuth
   module Strategies
     class SignicatOauth2 < OmniAuth::Strategies::OAuth2
       option :name, :signicat_oauth2
+      option :account
+      option :iss
+      option :response_type, 'code'
 
-      option :client_options, {}
-
-      option :scope, "openid profile email"
+      def setup_phase
+        super
+        options.client_options[:site] ||= "https://#{options.account}.signicat.com/auth/open"
+        options.client_options[:authorize_url] ||= "https://#{options.account}.signicat.com/auth/open/connect/authorize"
+        options.client_options[:token_url] ||= "https://#{options.account}.signicat.com/auth/open/connect/token"
+        options.iss ||= options.client_id
+      end
 
       uid { raw_info["sub"] }
 
@@ -24,9 +31,17 @@ module OmniAuth
         }
       end
 
-      def raw_info
-        @raw_info ||= access_token.get("/oidc/userinfo").parsed
+      def callback_url
+        options[:callback_url]
       end
+
+      def raw_info
+        userinfo_url = "https://#{options.account}.signicat.com/auth/open/connect/userinfo"
+        @raw_info ||= access_token.get(userinfo_url).parsed 
+
+      end
+
     end
   end
 end
+
